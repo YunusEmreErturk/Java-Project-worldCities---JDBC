c271416c7b796ac226f967b4d9f3982ae84f99ac
Yunus Emre Ertürk <45642601+YunusEmreErturk@users.noreply.github.com>
2019-06-02 17:09:54 +0300

    first commit all files project

---
+++ src/DbHelper.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+public class DbHelper {
+    private String userName = "root";
+    private String password = "mukoral012";
+    private String dbUrl="jdbc:mysql://localhost:3306/world?useSSL=false&serverTimezone=UTC";
+    
+    public Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection(dbUrl,userName,password);
+        
+    }
+    
+    public void showErrorMessage(SQLException exception)
+    {
+        System.out.println("Error Message : " + exception.getMessage());
+        System.out.println("Error Code : " + exception.getErrorCode());
+    }
+}
